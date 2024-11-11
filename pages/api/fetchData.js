41dc26229ecd75f33df6fc348db78aae10cd9022
Alex Leventis <alex.leventis@bit.ly>
2024-11-10 21:23:23 -0700

    cache-control no store

--- pages/api/fetchData.js
+++ pages/api/fetchData.js
@@ -1,24 +1,35 @@
+const safetySettings = [
+  { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_NONE" },
+  { category: "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold: "BLOCK_NONE" },
+  { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
+  { category: "HARM_CATEGORY_DANGEROUS_CONTENT", threshold: "BLOCK_NONE" },
+];
+
 export default async function handler(req, res) {
     const apiKey = process.env.REACT_APP_GEM_KEY;
   
     try {
       const prompt = req.body.prompt;
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
         {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0",
+            "Surrogate-Control": "no-store"
           },
           body: JSON.stringify({
             contents: [{ parts: [{ text: prompt }] }],
+            safetySettings: safetySettings
           }),
         }
       );
       const data = await response.json();
       res.status(200).json(data);
     } catch (error) {
-      console.log("async fetch error: ", error)
-      res.status(500).json({ error: "Error fetching dataa" });
-    }
+      res.status(500).json({ error: "Error fetching data: " + error});
+    };
   }
